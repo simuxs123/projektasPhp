49120a31a1ac25275dc834fa6f0917a32d428ed2
simonas <paulius.simonas@gmail.com>
2021-02-12 23:34:02 +0200

    "pirmas darbas"

---
+++ index.php
@@ -0,0 +1,162 @@
+<?php
+$klase=[
+    "5b"=>[
+        "mokinys1"=>[
+            "kodas"=>rand(0,100),
+            "vardas"=>"petras",
+            "pavarde"=>"petraitis",
+            "pazymiai"=>[
+                "matematika"=>5,
+                "istorija"=>10,
+                "lietuviu"=>2
+            ],
+            "data"=>date('Y-m-d H:i:s'),
+        ],
+        "mokinys2"=>[
+            "kodas"=>rand(0,100),
+            "vardas"=>"jonas",
+            "pavarde"=>"jonaitis",
+            "pazymiai"=>[
+                "matematika"=>5,
+                "istorija"=>10,
+                "lietuviu"=>2
+            ],
+            "data"=>date('Y-m-d H:i:s'),
+        ],
+        "mokinys3"=>[
+            "kodas"=>rand(0,100),
+            "vardas"=>"simas",
+            "pavarde"=>"simaitis",
+            "pazymiai"=>[
+                "matematika"=>5,
+                "istorija"=>10,
+                "lietuviu"=>2
+            ],
+            "data"=>date('Y-m-d H:i:s'),
+        ],
+        "mokinys4"=>[
+            "kodas"=>rand(0,100),
+            "vardas"=>"ignas",
+            "pavarde"=>"ignaitis",
+            "pazymiai"=>[
+                "matematika"=>5,
+                "istorija"=>10,
+                "lietuviu"=>3
+            ],
+            "data"=>date('Y-m-d H:i:s'),
+        ],
+        "mokinys5"=>[
+            "kodas"=>rand(0,100),
+            "vardas"=>"petras",
+            "pavarde"=>"petraitis",
+            "pazymiai"=>[
+                "matematika"=>5,
+                "istorija"=>10,
+                "lietuviu"=>8
+            ],
+            "data"=>date('Y-m-d H:i:s'),
+        ],
+        "mokinys6"=>[
+            "kodas"=>rand(0,100),
+            "vardas"=>"petras",
+            "pavarde"=>"petraitis",
+            "pazymiai"=>[
+                "matematika"=>9,
+                "istorija"=>10,
+                "lietuviu"=>9
+            ],
+            "data"=>date('Y-m-d H:i:s'),
+        ],
+        "mokinys7"=>[
+            "kodas"=>rand(0,100),
+            "vardas"=>"petras",
+            "pavarde"=>"petraitis",
+            "pazymiai"=>[
+                "matematika"=>5,
+                "istorija"=>9,
+                "lietuviu"=>8
+            ],
+            "data"=>date('Y-m-d H:i:s'),
+        ],
+        "mokinys8"=>[
+            "kodas"=>rand(0,100),
+            "vardas"=>"petras",
+            "pavarde"=>"petraitis",
+            "pazymiai"=>[
+                "matematika"=>5,
+                "istorija"=>9,
+                "lietuviu"=>6
+            ],
+            "data"=>date('Y-m-d H:i:s'),
+        ],
+        "mokinys9"=>[
+            "kodas"=>rand(0,100),
+            "vardas"=>"benas",
+            "pavarde"=>"benaitis",
+            "pazymiai"=>[
+                "matematika"=>6,
+                "istorija"=>5,
+                "lietuviu"=>7
+            ],
+            "data"=>date('Y-m-d H:i:s'),
+        ],
+        "mokinys10"=>[
+            "kodas"=>rand(0,100),
+            "vardas"=>"zigmas",
+            "pavarde"=>"zigmaitis",
+            "pazymiai"=>[
+                "matematika"=>5,
+                "istorija"=>5,
+                "lietuviu"=>6
+            ],
+            "data"=>date('Y-m-d H:i:s'),
+        ],
+    ]
+];
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <link type="text/css" rel="stylesheet" href="style.css" >
+    <title>Document</title>
+</head>
+<body>
+<table class="table">
+    <thead>
+    <tr>
+        <th>Klasė</th>
+        <th>Kodas</th>
+        <th>Vardas</th>
+        <th>Pavardė</th>
+        <th>Kontrolinių darbų vidurkis</th>
+        <th>Duomenų formavimo data</th>
+    </tr>
+    </thead>
+    <?php foreach($klase as $key=>$value): ?>
+        <?php $klas=$key?>
+        <?php foreach($value as $key=>$val): ?>
+            <tr>
+                <td><?= $klas; ?></td>
+                <?php $sum=0 ?>
+                <?php foreach ($val as $key=>$v):?>
+                    <?php if($key=="pazymiai"):?>
+                        <?php foreach ($val[$key] as $paz):?>
+                            <?php $sum+=$paz ?>
+                        <?php endforeach; ?>
+                    <td><?= round($sum/count($val[$key]),1) ?></td>
+                    <?php else : ?>
+                        <td><?= $val[$key]; ?></td>
+
+                    <?php endif;?>
+                <?php endforeach; ?>
+            </tr>
+        <?php endforeach; ?>
+    <?php endforeach; ?>
+</table>
+</body>
+</html>
+
